5853f6aca9ca6ccbaa844fc4abbe50bb5b990a72
Kallan Jones <kallan.jones@gmail.com>
2016-07-11 17:18:41 +1200

    Added a ball that bounces up and down :)

--- app/src/main/java/com/example/kallanjones/simplegame/MainActivity.java
+++ app/src/main/java/com/example/kallanjones/simplegame/MainActivity.java
@@ -1,13 +1,187 @@
 package com.example.kallanjones.simplegame;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    GameView gameView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Initialize gameView and set it as the view
+        gameView = new GameView(this);
+        setContentView(gameView);
+
+    }
+
+
+    // Here is our implementation of GameView
+    // It is an inner class.
+    // Note how the final closing curly brace }
+    // is inside SimpleGameEngine
+
+    // Notice we implement runnable so we have
+    // A thread and can override the run method.
+    class GameView extends SurfaceView implements Runnable {
+
+        // This is our thread
+        Thread gameThread = null;
+
+        // This is new. We need a SurfaceHolder
+        // When we use Paint and Canvas in a thread
+        // We will see it in action in the draw method soon.
+        SurfaceHolder ourHolder;
+
+        // A boolean which we will set and unset
+        // when the game is running- or not.
+        volatile boolean playing;
+
+        // A Canvas and a Paint object
+        Canvas canvas;
+        Paint paint;
+
+        // This variable tracks the game frame rate
+        long fps;
+        // This is used to help calculate the fps
+        private long timeThisFrame;
+
+        //Circle xy
+        int circleX = 500;
+        int circleY = 600;
+        int circleSize = 100;
+        int accelY = 10;
+
+        // When the we initialize (call new()) on gameView
+        // This special constructor method runs
+        public GameView(Context context) {
+            // The next line of code asks the
+            // SurfaceView class to set up our object.
+            // How kind.
+            super(context);
+
+            // Initialize ourHolder and paint objects
+            ourHolder = getHolder();
+            paint = new Paint();
+        }
+
+        @Override
+        public void run() {
+            while (playing) {
+
+                // Capture the current time in milliseconds in startFrameTime
+                long startFrameTime = System.currentTimeMillis();
+
+                // Update the frame
+                update();
+
+                // Draw the frame
+                draw();
+
+                // Calculate the fps this frame
+                // We can then use the result to
+                // time animations and more.
+                timeThisFrame = System.currentTimeMillis() - startFrameTime;
+                if (timeThisFrame > 0) {
+                    fps = 1000 / timeThisFrame;
+                }
+            }
+        }
+
+        public void update()
+        {
+            circleY += accelY;
+
+            if(circleY < 0)
+            {
+                accelY *= -1;
+            }
+            else if (circleY > getHeight())
+            {
+                accelY *= -1;
+            }
+        }
+
+        // If SimpleGameEngine Activity is paused/stopped
+        // shutdown our thread.
+        public void pause() {
+            playing = false;
+            try {
+                gameThread.join();
+            } catch (InterruptedException e) {
+                Log.e("Error:", "joining thread");
+            }
+        }
+
+        // If SimpleGameEngine Activity is started then
+        // start our thread.
+        public void resume() {
+            playing = true;
+            gameThread = new Thread(this);
+            gameThread.start();
+        }
+
+        // Draw the newly updated scene
+        public void draw() {
+
+            // Make sure our drawing surface is valid or we crash
+            if (ourHolder.getSurface().isValid()) {
+                // Lock the canvas ready to draw
+                // Make the drawing surface our canvas object
+                canvas = ourHolder.lockCanvas();
+
+                // Draw the background color
+                canvas.drawColor(Color.WHITE);
+
+                // Choose the brush color for drawing
+                paint.setColor(Color.BLACK);
+
+                // Make the text a bit bigger
+                paint.setTextSize(45);
+
+                // Display the current fps on the screen
+                canvas.drawText("FPS:    ", 50, 100, paint);
+                canvas.drawText("FPS:" + fps, 50, 100, paint);
+
+                paint.setColor(Color.RED);
+                canvas.drawCircle(circleX, circleY, circleSize, paint);
+
+                // Draw everything to the screen
+                // and unlock the drawing surface
+                ourHolder.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+
+    // This method executes when the player starts the game
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Tell the gameView resume method to execute
+        gameView.resume();
+    }
+
+    // This method executes when the player quits the game
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Tell the gameView pause method to execute
+        gameView.pause();
     }
 }
